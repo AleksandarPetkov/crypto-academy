77d68c06ab06c102be6702ed56f35559e461ce99
AleksandarPetkov <a.petkovpetkov@gmail.com>
2022-04-01 15:34:48 +0300

    Test

--- crypto-academy/src/main/java/com/app/cryptoacademy/security/SecurityConfig.java
+++ crypto-academy/src/main/java/com/app/cryptoacademy/security/SecurityConfig.java
@@ -8,16 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -43,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //Override login uri
         CustomAuthenticationFilter customFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customFilter.setFilterProcessesUrl("/api/login");
-        
+
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         http.authorizeRequests().antMatchers("/api/login").permitAll();
